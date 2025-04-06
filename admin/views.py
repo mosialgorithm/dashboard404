789dd0404e46b8a25b2c6cd7f72393a56a324699
mosiighorbani <mosi.php@gmail.com>
2025-04-06 23:05:02 +0330

    font vazir and yekan is addedd

--- admin/views.py
+++ admin/views.py
@@ -1,9 +1,17 @@
 from flask import render_template, redirect, url_for
 from . import admin
 from flask_login import login_required
+from account.models import UserModel
 
 
 @admin.route('/')
 @login_required
 def index():
-    return render_template('admin/index.html')
+    return render_template('admin/dashboard.html')
+
+
+@admin.route('/users')
+@login_required
+def users():
+    all_users = UserModel.query.all()
+    return render_template('admin/users/users-list-page.html', all_users=all_users)
